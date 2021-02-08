868d25e057f1ad1e3aa54db59cf0bc34715761d0
smartscore_smlee <smlee@smartscore.kr>
2021-02-08 16:03:22 +0900

    vue awesome swiper version update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import {Swiper as SwiperClass, Pagination, Navigation} from 'swiper/swiper.esm.js'
 
 
 Vue.config.productionTip = false
@@ -13,11 +12,8 @@ import './assets/scss/import.scss'
 // import style (>= Swiper 6.x)
 import 'swiper/swiper-bundle.css'
 
-// // import style (<= Swiper 5.x)
-// import 'swiper/css/swiper.css'
 
 Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
-SwiperClass.use([Pagination, Navigation])
 
 
 new Vue({
