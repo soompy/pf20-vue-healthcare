422de7920af6723b20b467bae7f2098fbd234565
soompy <sooper1137@gmail.com>
2021-03-03 18:35:46 +0900

    merge

--- src/views/page/Note/Note.vue
+++ src/views/page/Note/Note.vue
@@ -4,11 +4,9 @@
 
 <!--    더하기 버튼 클릭시-->
 <!--    노트 타입선택 > 시간설정 > 노트 입력페이지(제목, 이모티콘) > 저장 후 노트 등록완료-->
-    <round-button fillType="fill" @clickBtn="addNote"></round-button>
+    <round-button fillType="line" svgColor="grey" @clickBtn="addNote" :style="`position: fixed;right: 4vw;top: 70px;z-index: 5;`"></round-button>
 
-    <NoteList/>
-
-   <button class="round-button round_line_button" @click="addNote"><i class="ic_plus wh-26 grey"></i></button>
+    <NoteList/>  
 
   </div>
 </template>
@@ -55,9 +53,5 @@ import RoundButton from '../../components/ui/RoundButton'
 </script>
 
 <style lang="scss" scoped>
-  .round-button {
-    position: fixed;
-    top: 80px;
-    right: 4vw;
-  }
+
 </style>
