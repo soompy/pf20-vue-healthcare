774c6e73e731066c42e16ce2e466376db59a2ff9
smartscore_smlee <smlee@smartscore.kr>
2021-03-03 13:42:30 +0900

    리스트 grid로 바꿈

--- src/views/components/layouts/squareList.vue
+++ src/views/components/layouts/squareList.vue
@@ -1,72 +1,72 @@
 <template>
     <div class="cp_common_area">
         <ul class="square-list">
-      <li class="square-item">
-          <div class="list-item-title">
-              <div class="detail-item-info">
-                  <h3>Heart rate</h3>
-                  <span>Today, 04:27 PM</span>
+          <li class="square-item">
+              <div class="list-item-title">
+                  <div class="detail-item-info">
+                      <h3>Heart rate</h3>
+                      <span>Today, 04:27 PM</span>
+                  </div>
+                  <div class="title-icon heart-rate">
+                      <i class="ic_like wh-30 ss-pink"></i>
+                  </div>
               </div>
-              <div class="title-icon heart-rate">
-                  <i class="ic_like wh-30 ss-pink"></i>
+              <div class="item-graph">
+                  graph area
               </div>
-          </div>
-          <div class="item-graph">
-              graph area
-          </div>
-          <div class="item-fighre">
-              <strong>82</strong> bpm
-          </div>
-      </li>
-      <li class="square-item">
-          <div class="list-item-title">
-              <div class="detail-item-info">
-                  <h3>Water</h3>
-                  <span>Today, 04:27 PM</span>
+              <div class="item-fighre">
+                  <strong>82</strong> bpm
               </div>
-              <div class="title-icon water">
-                  <i class="ic_like wh-30 ss-blue"></i>
+          </li>
+          <li class="square-item">
+              <div class="list-item-title">
+                  <div class="detail-item-info">
+                      <h3>Water</h3>
+                      <span>Today, 04:27 PM</span>
+                  </div>
+                  <div class="title-icon water">
+                      <i class="ic_like wh-30 ss-blue"></i>
+                  </div>
               </div>
-          </div>
-          <div class="item-graph">
-              graph area
-          </div>
-      </li>
-      <li class="square-item">
-          <div class="list-item-title">
-              <div class="detail-item-info">
-                  <h3>Sleep</h3>
-                  <span>Today, 04:27 PM</span>
+              <div class="item-graph">
+                  graph area
               </div>
-              <div class="title-icon sleep">
-                  <i class="ic_like wh-30 ss-pink"></i>
+          </li>
+          <li class="square-item">
+              <div class="list-item-title">
+                  <div class="detail-item-info">
+                      <h3>Sleep</h3>
+                      <span>Today, 04:27 PM</span>
+                  </div>
+                  <div class="title-icon sleep">
+                      <i class="ic_like wh-30 ss-pink"></i>
+                  </div>
               </div>
-          </div>
-          <div class="item-graph">
-              graph area
-          </div>
-          <div class="item-fighre">
-              <strong>82</strong> bpm
-          </div>
-      </li>
-      <li class="square-item">
-          <div class="list-item-title">
-              <div class="detail-item-info">
-                  <h3>Weight</h3>
-                  <span>Today, 04:27 PM</span>
+              <div class="item-graph">
+                  graph area
               </div>
-              <div class="title-icon weight">
-                  <i class="ic_like wh-30 ss-pink"></i>
+              <div class="item-fighre">
+                  <strong>82</strong> bpm
               </div>
-          </div>
-          <div class="item-graph">
-              graph area
-          </div>
-          <div class="item-fighre">
-              <strong>82</strong> bpm
-          </div>
-      </li>
-    </ul>
+          </li>
+          <li class="square-item">
+              <div class="list-item-title">
+                  <div class="detail-item-info">
+                      <h3>Weight</h3>
+                      <span>Today, 04:27 PM</span>
+                  </div>
+                  <div class="title-icon weight">
+                      <i class="ic_like wh-30 ss-pink"></i>
+                  </div>
+              </div>
+              <div class="item-graph">
+                  graph area
+              </div>
+              <div class="item-fighre">
+                  <strong>82</strong> bpm
+              </div>
+          </li>
+        </ul>
     </div>
 </template>
 
@@ -84,21 +84,17 @@ export default {
 
 <style lang="scss" scoped>
     .square-list {
-        display: flex;
-        flex-wrap: wrap;
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        grid-template-columns: 1fr 1fr;
+        gap: 10px;
+        padding: 0 4vw;
         .square-item {
-            display: flex;
+            display: grid;
             flex-direction: column;
-            width: 44%;
-            height: 44vw;
             border: 1px solid #ccc;
             border-radius: 10px;
-            margin-left: 4vw;
-            margin-bottom: 4vw;
             padding: 10px;
-            &:nth-child(even) {
-                margin-right: 4vw;
-            }
             .list-item-title {
                 display: flex;
                 align-items: center;
--- src/views/page/Home/Home.vue
+++ src/views/page/Home/Home.vue
@@ -5,7 +5,9 @@
       <profile></profile>
       <div class="status-box">
         <div class="nickname-info"><strong>A</strong>님의 진행도</div>
-        <progress-bar></progress-bar>
+        <!-- 진척도 콘텐츠 -->
+        <auto-per></auto-per>
+        <!-- 진척도 콘텐츠 -->
       </div>
     </div>
     <!-- 상단 -->
@@ -14,25 +16,6 @@
 
     <square-list></square-list>
 
-    <div class="container100 mv-20 ph-4p">
-
-      <router-link to="/">홈</router-link> <br>
-      <router-link to="/about">소개</router-link> <br>
-      <router-link to="/notice">공지</router-link> <br>
-      <router-link to="/login">로그인</router-link> <br>
-      <router-link to="/mypage">마이페이지</router-link> <br>
-      <router-link to="/note">메모</router-link> <br>
-      <router-link to="/planner">플래너</router-link> <br>
-      <router-link to="/common-ui">공통유아이</router-link> <br>
-
-
-      <!-- 진척도 콘텐츠 -->
-      <auto-per></auto-per>
-      <!-- 진척도 콘텐츠 -->
-
-      <!-- 투데이 미션 -->
-      <h3 class="ta-l">Today</h3>
-    </div>
 
 
     <!-- 투데이 미션 -->
@@ -45,7 +28,6 @@
 
 
 import AutoPer from "../../components/swiper/auto-per";
-import ProgressBar from "../../components/graph/progress-bar";
 import Profile from "../../components/layouts/profile";
 import SquareList from '../../components/layouts/squareList.vue';
 import MiniCalendar from '../../components/layouts/miniCalendar.vue';
@@ -54,7 +36,6 @@ export default {
   name: 'Home',
   components: {
     Profile,
-    ProgressBar,
     AutoPer,
     SquareList,
     MiniCalendar,
