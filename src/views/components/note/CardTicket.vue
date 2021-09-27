3aef4a24d229abd20bfa00b3d8d2513ce64df4c7
smartscore_smlee <smlee@smartscore.kr>
2021-09-27 18:01:17 +0900

    removeclass card

--- src/views/components/note/CardTicket.vue
+++ src/views/components/note/CardTicket.vue
@@ -9,7 +9,7 @@
           </div>
         </div>
       </div>
-      <div class="content" v-on:click="addClass" :class="{'full': isAddClass}" @close="closeContent">
+      <div class="content" v-on:click="addClass" :class="{'full': isAddClass}">
         <div class="articlemeta">
           <ul class="left">
             <li><i class="material-icons">&#xE15E;</i></li>
@@ -21,7 +21,7 @@
         </div>
         <div class="closebar">
           <h1>오늘의 건강 뉴스</h1>
-          <a class="closebttn" @click="$emit('close')"><i class="material-icons">&#xE5CD;</i></a>
+          <a class="closebttn" @click="$emit('deleteClass', false)"><i class="material-icons">&#xE5CD;</i></a>
         </div>
 
         <div class="readmore">
@@ -46,52 +46,6 @@
       </div>
     </div>
   </div>
-
-<!--  <div class="flex">-->
-<!--    <div class="card">-->
-<!--      <div class="header">-->
-<!--        <div class="title">-->
-<!--          <div>-->
-<!--            <h1>5 ways to create inspiring articles</h1>-->
-<!--            <h6><span>by</span> Joe Bloggs</h6>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      <div class="content">-->
-<!--        <div class="articlemeta">-->
-<!--          <ul class="left">-->
-<!--            <li><i class="material-icons">&#xE15E;</i></li>-->
-<!--            <li><i class="material-icons">&#xE87D;</i></li>-->
-<!--          </ul>-->
-<!--          <ul class="right">-->
-<!--            <li><i class="material-icons">&#xE80D;</i></li>-->
-<!--          </ul>-->
-
-
-<!--        </div>-->
-<!--        <div class="closebar">-->
-<!--          <h1>5 ways to create...</h1>-->
-<!--          <a href="#0" class="closebttn"><i class="material-icons">&#xE5CD;</i></a>-->
-<!--        </div>-->
-<!--        <p>Isn't it great to do something you can't fail at? Work on one thing at a time. Don't get carried away - we have plenty of time. You can do it. If we're gonna walk though the woods, we need a little path. You have to make these big decisions.</p>-->
-
-<!--        <p>We don't really know where this goes - and I'm not sure we really care. Get away from those little Christmas tree things we used to make in school. From all of us here, I want to wish you happy painting and God bless, my friends. If you don't think every day is a good day - try missing a few. You'll see.</p>-->
-
-<!--        <p>Do an almighty painting with us. Remember how free clouds are. They just lay around in the sky all day long. When you do it your way you can go anywhere you choose. Put your feelings into it, your heart, it's your world. You can't have light without dark. You can't know happiness unless you've known sorrow.</p>-->
-
-<!--        <p>Use absolutely no pressure. Just like an angel's wing. These trees are so much fun. I get started on them and I have a hard time stopping. Every day I learn.</p>-->
-
-<!--        <img src="http: //placehold.it/380x200">-->
-
-
-<!--        <p>There's not a thing in the world wrong with washing your brush. You've got to learn to fight the temptation to resist these things. Just let them happen. Painting should do one thing. It should put happiness in your heart. I can't think of anything more rewarding than being able to express yourself to others through....-->
-<!--        </p>-->
-<!--        <a href="#0" class="bttn">Read Full Article</a>-->
-<!--        <a href="#0" class="readmore" v-on: click="addClass" : class="{'myClass': isAddClass}" ></a>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-
 <!--  https: //codepen.io/kitsune/pen/zKzRXQ-->
 </template>
 
@@ -102,18 +56,18 @@
     data() {
       return {
         isAddClass: false,
-        isModalVisible: false,
+        isRemoveClass: false,
       }
     },
     components: {
 
     },
     methods: {
-      addClass: function() {
+      addClass() {
         this.isAddClass = true;
       },
-      closeContent() {
-        this.isAddClass = false;
+      deleteClass() {
+        this.isRemoveClass = true;
       },
     }
   }
@@ -282,6 +236,7 @@
           position: absolute;
           right: 15px;
           top: -2px;
+          cursor: pointer;
         }
         h1 {
           transition: all .4s cubic-bezier(.4,0,.2,1) .3s;
